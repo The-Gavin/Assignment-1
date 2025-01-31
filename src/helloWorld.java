e90c344fa3783a6275a1f0f1de5f014d28832266
BryanShea0207 <Bryanshea0207@gmail.com>
2025-01-30 21:15:39 -0500

    Update helloWorld.java

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -4,10 +4,10 @@ public class helloWorld {
 	public static void main(String[] args) {
 		int x = 2;
 		int y = 7;
-		System.out.println(breakRule(y,x));
+		System.out.println(breakRule(y, x));
 	}
 	
-	public static int breakRule(int x,int y) {
+	public static int breakRule(int x, int y) {
 		return x + y;
 	}
 }

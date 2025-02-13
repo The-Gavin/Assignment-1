ec4625ea900e613905c4c9762c9b04b3576bf0e3
BryanShea0207 <Bryanshea0207@gmail.com>
2025-02-13 11:17:43 -0500

    Update PrototypeProcess.java
    Fixed incorrect styling

--- src/PrototypeProcess.java
+++ src/PrototypeProcess.java
@@ -2,8 +2,7 @@ import project.annotations.ProcessAPIPrototype;
 
 public class PrototypeProcess {
 	@ProcessAPIPrototype
-	public void prototype(Processing readWriter)
-	{
+	public void prototype(Processing readWriter) {
 		//Read from output
 		ReadData outputSource = readWriter.readMethod();
 		//Write to input

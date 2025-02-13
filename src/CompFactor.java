cbe8567861f7c54b3b7a0bf9f7fe74abd129f4ce
BryanShea0207 <Bryanshea0207@gmail.com>
2025-02-13 12:30:33 -0500

    Annotations fix (#20)
    * Office Hours Work
    
    Annotations added to src folder, prototype API file created and started, interface file started.
    
    * Revised Annotations
    
    got rid of the sample code in prototype API
    
    * Created Network API file
    
    Moved code from "PrototypeFactor" to new file called" PrototypeNetwork" for work on the first API, the network between the user and the compute engine.
    
    * Finished draft for Conceptual Prototype
    
    Finished draft for Conceptual Prototype
    Sorted draft for Network Prototype
    Finished draft for Conceptual Interface
    Sorted draft for Network Interface
    
    * Changed "Engine" to "CompFactor"
    
    Renamed uses of the word "engine" to "compfactor" for the conceptual API to avoid confusion
    
    * Finished Process API
    
    Created interface for Process API
    Created Prototype file for Process API
    
    Cleaned up readability and comments in Conceptual and Network APIs
    
    * Annotation fixed (#19)
    
    * Create README.md (#16)
    
    Created the README and included a discription of the choosen computation with examples
    
    * Added System Diagram to README.md (#17)
    
    * Add System Diagram to SRC
    
    adding the System Diagram image in order to link it to the README.md
    
    * Update README.md
    
    Added the Image of the System Diagram into the README.md
    
    * Update README.md (#18)
    
    * Update README.md
    
    Needed to change the URL on the image now that the System Diagram image is in the main branch
    
    * Update README.md
    
    * fixed annotation
    
    added @retention annotation to annotation interfaces
    
    * Update PrototypeProcess.java
    
    Fixed incorrect styling
    
    * Update Assignment2StructureTest.java
    
    removed unused imports that potentially stopped merge
    
    * Update build.gradle
    
    Added setFailOnNoMatchingTests(false) to fix build failing
    
    ---------
    
    Co-authored-by: Gavin is Lightning <peartg1@newpaltz.edu>

---
+++ src/CompFactor.java
@@ -0,0 +1,9 @@
+import project.annotations.ConceptualAPI;
+
+@ConceptualAPI
+public interface CompFactor {
+	// output the integer followed by each of its factors
+	public String printFactor();
+	// find all the factors of the integers in order
+	public int[] factorCalc();
+}

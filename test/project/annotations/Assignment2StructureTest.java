491c29668ff68cd7797279a5724f3a374ee3ada8
Gavin is Lightning <peartg1@newpaltz.edu>
2025-03-10 03:48:10 -0400

    Removed usless imports
    Attempting to resolve conflicts in pull request

--- test/project/annotations/Assignment2StructureTest.java
+++ test/project/annotations/Assignment2StructureTest.java
@@ -12,13 +12,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import src.project.annotations.ConceptualAPI;
-import src.project.annotations.ConceptualAPIPrototype;
-import src.project.annotations.NetworkAPI;
-import src.project.annotations.NetworkAPIPrototype;
-import src.project.annotations.ProcessAPI;
-import src.project.annotations.ProcessAPIPrototype;
-
 /**
  * This test checks that all 3 APIs exist in the 'src' folder as interfaces, with the appropriate annotations, and that they all
  * have corresponding prototypes, also with the appropriate annotations.

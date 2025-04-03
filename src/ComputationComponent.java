1c294f492205767a852be75f3a0f0b1ea6dc8c7a
BryanShea0207 <Bryanshea0207@gmail.com>
2025-04-03 19:19:21 -0400

    implemented Compute Engine (#35)
    * implemented Compute Engine
    
    * fixed checkstyle

--- src/ComputationComponent.java
+++ src/ComputationComponent.java
@@ -1,3 +1,8 @@
 public class ComputationComponent {
+
+	public InitializationResponse initialize() {
+		// TODO Auto-generated method stub
+		return new InitializationResponse(Response.Status.SUCCESS);
+	}
     //TBD
 }
--- src/StreamSource.java
+++ src/StreamSource.java
@@ -1,3 +1,8 @@
 public class StreamSource {
+
+	public ReadStreamResponse containsData() {
+		// TODO Auto-generated method stub
+		return new ReadStreamResponse(Response.Status.SUCCESS);
+	}
     //TBD
 }
